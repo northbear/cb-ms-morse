052ebd315b291c215137ebd720ef4b572c12d77d
Igor Yurchenko <harry.urcen@gmail.com>
2020-09-11 20:57:33 +0300

    add getting listening port number from evnvar APP_PORT

--- morse-responder/morse-responder.go
+++ morse-responder/morse-responder.go
@@ -5,12 +5,27 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"strconv"
 )
+
+const DEFAULT_APP_PORT = "5000"
+
+func MorseEncoder(m string) string {
+	return fmt.Sprintf("Fake morse encoding for message: %s", m)
+}
 
 func main() {
 	fmt.Println("Hello, World from Responder!")
+	port := os.Getenv("APP_PORT")
+	_, err := strconv.ParseInt(port, 10, 32)
+	if err != nil {
+		log.Printf("Warning: Cannot parse value of envvar APP_PORT: %s", port)
+		log.Printf("Use default value: %s", DEFAULT_APP_PORT)
+		port = DEFAULT_APP_PORT
+	}
 	server := &http.Server{
-		Addr: ":5000",
+		Addr: ":" + port,
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +36,6 @@ func main() {
 		fmt.Fprintf(w, "Hello, World from Responder! %q\n", ip)
 		log.Printf("%+v\n", r)
 	})
-
+	log.Printf("Listen to port: %s", port)
 	log.Fatal(server.ListenAndServe())
 }
