c35ea0a0d5bdc449850c65c1f71ed06bf9968a67
Igor Yurchenko <harry.urcen@gmail.com>
2020-09-12 20:40:48 +0300

    add getting service address from envvar

--- morse-receiver/morse-receiver.go
+++ morse-receiver/morse-receiver.go
@@ -3,16 +3,32 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"os"
+)
+
+const (
+	NUM_TRYS = 10
+	PAUSE    = 10
 )
 
 func main() {
-	fmt.Println("Hello, World from Receiver!")
-	resp, err := http.Get("http://localhost:5000/")
+	servaddr := os.Getenv("SERVICE_ADDRESS")
+	_, _, err := net.SplitHostPort(servaddr)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error: wrong value of SERVICE_ADDRESS envvar")
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println(body)
+	url := fmt.Sprintf("http://%s/", servaddr)
+	fmt.Println(url)
+
+	for i := 0; i < NUM_TRYS; i++ {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Printf("received: %s\n", body)
+	}
 }
