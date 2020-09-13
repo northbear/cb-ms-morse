6bc9a60b3ede92ea1dc54654f8f7d753d8928995
Igor Yurchenko <harry.urcen@gmail.com>
2020-09-13 17:25:28 +0300

    full implementation of morse-responder

--- morse-responder/morse-responder.go
+++ morse-responder/morse-responder.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"io"
 	"log"
 	"net"
 	"net/http"
@@ -11,31 +12,55 @@ import (
 
 const DEFAULT_APP_PORT = "5000"
 
-func MorseEncoder(m string) string {
-	return fmt.Sprintf("Fake morse encoding for message: %s", m)
+type MorseEncoderTable map[rune]string
+
+var encoderTable MorseEncoderTable = MorseEncoderTable{
+	'a': "*-", 'b': "-***", 'c': "-*-*", 'd': "-**",
+	'e': "*", 'f': "**-*", 'g': "--*", 'h': "****",
+	'i': "**", 'j': "J*---", 'k': "-*-", 'l': "*-**",
+	'm': "--", 'n': "-*", 'o': "---", 'p': "*--*",
+	'q': "--*-", 'r': "*-*", 's': "***", 't': "-",
+	'u': "**-", 'v': "***-", 'w': "*--", 'x': "-**-",
+	'y': "-*--", 'z': "--**",
+	'1': "*----", '2': "**---", '3': "***--", '4': "****-",
+	'5': "*****", '6': "-****", '7': "--***", '8': "---**",
+	'9': "----*", '0': "-----",
+	'.': "*-*-*-", ',': "--**--", '?': "**--**", '\'': "*---*",
+	'!': "-*-*--", '/': "-**-*", ':': "---***", ';': "-*-*-*",
+	'=': "-***-", '+': "*-*-*", '-': "-****-", '_': "**--*-",
+	'"': "*-**-*", '@': "*--*-*",
+}
+
+func MorseEncoder(w io.Writer, m string) error {
+	var encoded = bufio.NewWriter(w)
+	for _, c := range m {
+		encoded.WriteString(encoderTable[c])
+		encoded.WriteString(" ")
+	}
+	encoded.Flush()
+	return nil
 }
 
 func main() {
-	fmt.Println("Hello, World from Responder!")
+	log.Println("Start Morse Responder... 73.")
 	port := os.Getenv("APP_PORT")
 	_, err := strconv.ParseInt(port, 10, 32)
 	if err != nil {
-		log.Printf("Warning: Cannot parse value of envvar APP_PORT: %s", port)
+		log.Printf("Incorrect value of APP_PORT environment variable")
 		log.Printf("Use default value: %s", DEFAULT_APP_PORT)
 		port = DEFAULT_APP_PORT
 	}
-	server := &http.Server{
-		Addr: ":" + port,
-	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Got request from %s", r.RemoteAddr)
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
+			log.Println("Cannot detect remote address. Use default value: 0.0.0.0")
 			ip = "0.0.0.0"
 		}
-		fmt.Fprintf(w, "Hello, World from Responder! %q\n", ip)
-		log.Printf("%+v\n", r)
+		MorseEncoder(w, ip)
 	})
+
 	log.Printf("Listen to port: %s", port)
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
